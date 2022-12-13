eec94314ddcf1e65472ac01609ab542357c6e089
PGabriel-MB <pablogrbarbosa@gmail.com>
2022-12-13 07:04:10 -0300

    desenvolvido loader da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { SplashLoading } from '../components/splash'
+import { SplashLoading } from '../components/splashLoading'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
